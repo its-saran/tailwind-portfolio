811bc78c197c46bc2a9ca811b74fce3ab9b01538
its-saran <its.saran.pv@gmail.com>
2023-07-31 18:24:41 +0530

    Added themes

---
+++ public/js/script.js
@@ -0,0 +1,41 @@
+// Smooth scroll
+document.addEventListener('DOMContentLoaded', function() {
+    var navLinks = document.querySelectorAll('.navbar a');
+  
+    for (var i = 0; i < navLinks.length; i++) {
+      navLinks[i].addEventListener('click', smoothScroll);
+    }
+  
+    function smoothScroll(e) {
+      e.preventDefault();
+  
+      var targetId = this.getAttribute('href');
+      var targetPosition = document.querySelector(targetId).offsetTop;
+      var startPosition = window.pageYOffset;
+      var distance = targetPosition - startPosition;
+      var duration = 1000;
+      var start = null;
+  
+      function animation(currentTime) {
+        if (start === null) {
+          start = currentTime;
+        }
+        var timeElapsed = currentTime - start;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) {
+          requestAnimationFrame(animation);
+        }
+      }
+  
+      function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+      }
+  
+      requestAnimationFrame(animation);
+    }
+});
+  

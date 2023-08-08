452373abeb64097631d37227eebae92db2afc78c
its-saran <its.saran.pv@gmail.com>
2023-08-08 12:20:41 +0530

    Add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
-  plugins: [require("daisyui")],
+  theme: {
+    fontFamily: {
+      oufit: ["Outfit", "sans-serif"]
+    }
+  },
+  plugins: [require("daisyui"), require('@tailwindcss/typography')],
   daisyui: {
     themes: [
-      "light",
-      "dark",
-      "dracula",
       {
-        mytheme: {
+        dark: {
           "primary": "#641ae6",
           "secondary": "#d926a9",
           "accent": "#1fb2a6",
@@ -19,6 +21,20 @@ module.exports = {
           "success": "#36d399",
           "warning": "#fbbd23",
           "error": "#f87272",
+        },
+        light: {
+          "primary": "#570df8",
+          "primary-content": "#E0D2FE",
+          "secondary": "#f000b8",
+          "secondary-content": "#FFD1F4",
+          "accent": "#1ECEBC",
+          "accent-content": "#07312D",
+          "neutral": "#2B3440",
+          "neutral-content": "#D7DDE4",
+          "base-100": "#ffffff",
+          "base-200": "#F2F2F2",
+          "base-300": "#E5E6E6",
+          "base-content": "#1f2937",
         },
       },
     ],

4b450b6ba2488660b8530f574485267dc02efe58
its-saran <its.saran.pv@gmail.com>
2023-08-10 23:34:46 +0530

    Change projects style

--- public/js/script.js
+++ public/js/script.js
@@ -95,26 +95,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const isotopeOptions = {
     itemSelector: ".element-item",
     layoutMode: "fitRows",
-    getSortData: {
-      name: ".name",
-      symbol: ".symbol",
-      number: ".number parseInt",
-      category: "[data-category]",
-      weight: function (itemElem) {
-        const weight = itemElem.querySelector(".weight").textContent;
-        return parseFloat(weight.replace(/[\(\)]/g, ""));
-      },
-    }
   };
   const isotopeInstance = new Isotope(squareSection, isotopeOptions);
-
-  // const filterButtons = document.querySelectorAll("#filters button");
-  // filterButtons.forEach(function (button) {
-  //   button.addEventListener("click", function () {
-  //     const filterValue = button.getAttribute("data-filter");
-  //     isotopeInstance.arrange({ filter: filterValue });
-  //   });
-  // });
 
   const daisyFilterButtons = document.querySelectorAll("#daisy-filters input");
 
@@ -126,24 +108,6 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
   });
-
-
-
-  // const buttonGroups = document.querySelectorAll(".button-group");
-  // buttonGroups.forEach(function (buttonGroup) {
-  //   buttonGroup.addEventListener("click", function (event) {
-  //     if (
-  //       event.target &&
-  //       event.target.nodeName === "BUTTON"
-  //     ) {
-  //       const buttons = buttonGroup.querySelectorAll("button");
-  //       buttons.forEach(function (button) {
-  //         button.classList.remove("is-checked");
-  //       });
-  //       event.target.classList.add("is-checked");
-  //     }
-  //   });
-  // });
 
 });
 

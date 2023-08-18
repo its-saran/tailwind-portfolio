1226e1a70d072eee953441a3b6c995673847a265
Saran <user.name>
2023-08-18 22:16:30 +0530

    Minor font fix

--- public/js/script.js
+++ public/js/script.js
@@ -154,6 +154,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+
   // document.getElementById('contact-form').addEventListener('submit', function(event) {
   //   event.preventDefault();
 

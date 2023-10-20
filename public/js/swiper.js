364efce7416d21326e06ed281797bf13117f073e
unknown <its.saran.pv@gmail.com>
2023-10-20 19:09:51 +0530

    Update project main and thumbnail image

---
+++ public/js/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
